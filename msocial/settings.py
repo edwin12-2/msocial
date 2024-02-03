2c271c93528f63ff35cfb886c3dd728cb869a246
edwin12-2 <80588520+edwin12-2@users.noreply.github.com>
2024-02-03 07:45:55 -0500

    render

--- msocial/settings.py
+++ msocial/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = [*]
 
 INSTALLED_APPS = [
     'social.apps.SocialConfig',
+    'render.apps.RenderConfig',
     'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
